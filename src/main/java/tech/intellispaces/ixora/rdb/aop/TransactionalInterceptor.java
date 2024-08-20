8af4ae18c0a485984f96e1c61e99acd8e268df25
lavor <lavor.andrey@gmail.com>
2024-08-20 12:24:17 +0600

    Update

--- src/main/java/tech/intellispaces/ixora/rdb/aop/TransactionalInterceptor.java
+++ src/main/java/tech/intellispaces/ixora/rdb/aop/TransactionalInterceptor.java
@@ -3,14 +3,14 @@ package tech.intellispaces.ixora.rdb.aop;
 import intellispaces.ixora.rdb.TransactionFactoryHandle;
 import intellispaces.ixora.rdb.exception.TransactionException;
 import tech.intellispaces.actions.Action;
-import tech.intellispaces.core.aop.InterceptorAdvice;
+import tech.intellispaces.core.aop.Interceptor;
 import tech.intellispaces.core.system.Modules;
 import tech.intellispaces.ixora.rdb.TransactionFunctions;
 import tech.intellispaces.javastatements.method.MethodStatement;
 
 import java.util.List;
 
-public class TransactionalInterceptor extends InterceptorAdvice {
+public class TransactionalInterceptor extends Interceptor {
 
   public TransactionalInterceptor(MethodStatement joinPoint, Action nextAction) {
     super(joinPoint, nextAction);
@@ -19,8 +19,8 @@ public class TransactionalInterceptor extends InterceptorAdvice {
   @Override
   public Object execute(Object[] data) {
     TransactionFactoryHandle transactionFactory = getDefaultTransactionFactory();
-    Action interceptedAction = interceptedAction();
-    return TransactionFunctions.transactional(transactionFactory, interceptedAction::execute, data);
+    Action joinAction = joinAction();
+    return TransactionFunctions.transactional(transactionFactory, joinAction::execute, data);
   }
 
   private TransactionFactoryHandle getDefaultTransactionFactory() {
