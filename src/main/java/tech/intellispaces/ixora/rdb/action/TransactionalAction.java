91f5890a8f9c0b40abd37e124962ced6333319e0
lavor <lavor.andrey@gmail.com>
2024-08-22 13:14:27 +0600

    Update

--- src/main/java/tech/intellispaces/ixora/rdb/action/TransactionalAction.java
+++ src/main/java/tech/intellispaces/ixora/rdb/action/TransactionalAction.java
@@ -1,21 +1,20 @@
 package tech.intellispaces.ixora.rdb.action;
 
 import intellispaces.ixora.rdb.TransactionFactoryHandle;
-import tech.intellispaces.actions.AbstractAction;
 import tech.intellispaces.actions.Action;
+import tech.intellispaces.actions.wrapper.AbstractWrapper;
 import tech.intellispaces.ixora.rdb.TransactionFunctions;
 
-public class TransactionalAction extends AbstractAction {
+public class TransactionalAction extends AbstractWrapper {
   private final TransactionFactoryHandle transactionFactory;
-  private final Action internalAction;
 
-  public TransactionalAction(TransactionFactoryHandle transactionFactory, Action internalAction) {
+  public TransactionalAction(TransactionFactoryHandle transactionFactory, Action wrappedAction) {
+    super(wrappedAction);
     this.transactionFactory = transactionFactory;
-    this.internalAction = internalAction;
   }
 
   @Override
   public Object execute(Object... data) {
-    return TransactionFunctions.transactional(transactionFactory, internalAction::execute, data);
+    return TransactionFunctions.transactional(transactionFactory, wrappedAction()::execute, data);
   }
 }
