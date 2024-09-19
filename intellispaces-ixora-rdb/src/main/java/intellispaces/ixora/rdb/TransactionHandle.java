b28e13c4c078a45b9672774e659c08ec324e8b30
lavor <lavor.andrey@gmail.com>
2024-09-19 16:19:06 +0600

    Update TransactionHandle implementation

--- intellispaces-ixora-rdb/src/main/java/intellispaces/ixora/rdb/TransactionHandle.java
+++ intellispaces-ixora-rdb/src/main/java/intellispaces/ixora/rdb/TransactionHandle.java
@@ -67,9 +67,9 @@ public abstract class TransactionHandle implements MovableTransaction {
   @Mapper
   @Override
   public <D> D fetchData(Class<D> dataType, String query, Map<String, Object> params) {
-    PreparedQuery preparedQuery = prepareQuery(query, params);
-    PreparedStatement ps = connection.createPreparedStatement(preparedQuery.query());
-    setParamValues(ps, preparedQuery.values());
+    BlindQueryAndParameterNames blindQueryAndParamNames = prepareQuery(query, params);
+    PreparedStatement ps = connection.createPreparedStatement(blindQueryAndParamNames.query());
+    setParamValues(ps, blindQueryAndParamNames.paramNames(), params);
     ResultSet rs = ps.executeQuery();
     return fetchData(dataType, rs);
   }
@@ -85,35 +85,35 @@ public abstract class TransactionHandle implements MovableTransaction {
     return data;
   }
 
-  private PreparedQuery prepareQuery(String query, Map<String, Object> params) {
-    List<Object> values = new ArrayList<>();
+  private BlindQueryAndParameterNames prepareQuery(String query, Map<String, Object> params) {
     char[] originQuery = query.toCharArray();
-    char[] preparedQuery = new char[query.length()];
+    char[] blindQuery = new char[query.length()];
+    List<String> paramNames = new ArrayList<>();
     int index1 = 0, index2 = 0;
     while (index1 < originQuery.length) {
       char ch = originQuery[index1++];
       if (ch == ':') {
-        preparedQuery[index2++] = '?';
+        blindQuery[index2++] = '?';
         int ind = index1;
         while (index1 < originQuery.length && Character.isLetterOrDigit(originQuery[index1])) {
           index1++;
         }
         String paramName = query.substring(ind, index1);
-        if (!params.nativeMap().containsKey(paramName)) {
-          throw RdbException.withMessage("Value of parameter {0} is not found", paramName);
-        }
-        Object value = params.nativeMap().get(paramName);
-        values.add(value);
+        paramNames.add(paramName);
       } else {
-        preparedQuery[index2++] = ch;
+        blindQuery[index2++] = ch;
       }
     }
-    return new PreparedQuery(new String(preparedQuery, 0, index2), values);
+    return new BlindQueryAndParameterNames(new String(blindQuery, 0, index2), paramNames);
   }
 
-  private void setParamValues(PreparedStatement ps, List<Object> paramValues) {
+  private void setParamValues(PreparedStatement ps, List<String> paramNames, Map<String, Object> params) {
     int index = 1;
-    for (Object paramValue : paramValues) {
+    for (Object paramName : paramNames) {
+      if (!params.nativeMap().containsKey(paramName)) {
+        throw RdbException.withMessage("Value of parameter {0} is not found", paramName);
+      }
+      Object paramValue = params.nativeMap().get(paramName);
       if (paramValue instanceof Integer) {
         ps.setInt(index++, (int) paramValue);
       } else {
@@ -122,21 +122,21 @@ public abstract class TransactionHandle implements MovableTransaction {
     }
   }
 
-  private final static class PreparedQuery {
+  private final static class BlindQueryAndParameterNames {
     private final String query;
-    private final List<Object> values;
+    private final List<String> paramNames;
 
-    public PreparedQuery(String query, List<Object> values) {
+    public BlindQueryAndParameterNames(String query, List<String> paramNames) {
       this.query = query;
-      this.values = values;
+      this.paramNames = paramNames;
     }
 
     public String query() {
       return query;
     }
 
-    public List<Object> values() {
-      return values;
+    public List<String> paramNames() {
+      return paramNames;
     }
   }
 }
